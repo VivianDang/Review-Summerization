dab04c9f11197b30969d8af1484dcfa49b91b618
YuanDang <viviandang121@gmail.com>
2023-04-13 20:20:53 -0400

    Update abs_summarization_Bert2.py

--- abs_summarization_Bert2.py
+++ abs_summarization_Bert2.py
@@ -1,43 +1,95 @@
+import random
+
 import datasets
 import nltk
 import numpy as np
 import pandas as pd
-from accelerate import init_empty_weights
-from transformers import AutoConfig, AutoModelForCausalLM
+# from accelerate import init_empty_weights
+# from transformers import AutoConfig, AutoModelForCausalLM
+import torch
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, \
     Seq2SeqTrainer
 
 PATH = 'clean_data/train.csv'
 ckpt = 't5-small'
+SEED = 666
+
+def set_seeds(seed):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+set_seeds(SEED)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+torch.cuda.empty_cache()
+print(f'DEVICE: {device}')
+
+from pynvml import *
+
+
+def print_gpu_utilization():
+    nvmlInit()
+    handle = nvmlDeviceGetHandleByIndex(0)
+    info = nvmlDeviceGetMemoryInfo(handle)
+    print(f"GPU memory occupied: {info.used//1024**2} MB.")
+
+print_gpu_utilization()
+
 train_set = pd.read_csv(PATH, header=0, index_col=[0])
 train_data = datasets.Dataset.from_pandas(train_set.iloc[[i for i in range(len(train_set)) if i % 5 != 0]],
-                                          preserve_index=False)
+                                          preserve_index=False).select(range(10000))
 val_data = datasets.Dataset.from_pandas(train_set.iloc[[i for i in range(len(train_set)) if i % 5 == 0]],
-                                          preserve_index=False)
+                                          preserve_index=False).select(range(3000))
 tokenizer = AutoTokenizer.from_pretrained(ckpt)
 
 
+
 encoder_max_length = 512
-decoder_max_length = 64
-BATCH_SIZE = 64
+decoder_max_length = 40
+BATCH_SIZE = 16
 
 if ckpt in ["t5-small", "t5-base", "t5-larg", "t5-3b", "t5-11b"]:
     prefix = "summarize: "
 else:
     prefix = ""
 
-def tokenize_function(entry):
-    inputs = [prefix + doc for doc in entry['text']]
-    model_inputs = tokenizer(inputs, truncation=True, max_length=encoder_max_length)
-    # print(inputs)
-    with tokenizer.as_target_tokenizer():
-        labels = tokenizer(entry["summary"], truncation=True, max_length=decoder_max_length)
+# def tokenize_function(entry):
+#     inputs = [prefix + doc for doc in entry['text']]
+#     model_inputs = tokenizer(inputs, truncation=True, max_length=encoder_max_length)
+#     # print(inputs)
+#     with tokenizer.as_target_tokenizer():
+#         labels = tokenizer(entry["summary"], truncation=True, max_length=decoder_max_length)
+#
+#     model_inputs['labels'] = labels['input_ids'].copy()
+#     model_inputs['labels'] = [[-100 if token == tokenizer.pad_token_id else token for token in labels]
+#                           for labels in model_inputs['labels']]
+#
+#     return model_inputs
 
-    model_inputs['labels'] = labels['input_ids'].copy()
-    model_inputs['labels'] = [[-100 if token == tokenizer.pad_token_id else token for token in labels]
-                          for labels in model_inputs['labels']]
+def tokenize_function(batch):
+  # tokenize the inputs and labels
+  # print(batch)
+  inputs = tokenizer(batch["text"], truncation=True, max_length=encoder_max_length)
+  # print(inputs)
+  outputs = tokenizer(batch["summary"], truncation=True, max_length=decoder_max_length)
+  # print(outputs)
 
-    return model_inputs
+  batch["input_ids"] = inputs.input_ids
+  batch["attention_mask"] = inputs.attention_mask
+  # batch["decoder_input_ids"] = outputs.input_ids
+  # batch["decoder_attention_mask"] = outputs.attention_mask
+  batch["labels"] = outputs.input_ids.copy()
+
+#   # because RoBERTa automatically shifts the labels, the labels correspond exactly to `decoder_input_ids`.
+#   # We have to make sure that the PAD token is ignored
+  batch["labels"] = [[-100 if token == tokenizer.pad_token_id else token for token in labels] for labels in batch["labels"]]
+
+  return batch
 
 train_data = train_data.map(
     tokenize_function,
@@ -55,13 +107,14 @@ val_data = val_data.map(
 
 
 model = AutoModelForSeq2SeqLM.from_pretrained(ckpt)
+print_gpu_utilization()
 
 # freeze self-attention layer of encoder
 freeze_layers = [model.encoder.block[i].layer[0] for i in range(len(model.encoder.block)-1)]
 # freeze self-attention layer of decoder
 freeze_layers.extend([model.decoder.block[i].layer[0] for i in range(len(model.decoder.block)-1)])
 # freeze cross-attention layer
-freeze_layers.extend([model.decoder.block[i].layer[1] for i in range(len(model.decoder.block))])
+freeze_layers.extend([model.decoder.block[i].layer[1] for i in range(len(model.decoder.block)-1)])
 
 for module in freeze_layers:
     for param in module.parameters():
@@ -77,7 +130,8 @@ training_args = Seq2SeqTrainingArguments(
     per_device_eval_batch_size=BATCH_SIZE,
     weight_decay=0.01,
     fp16=True,
-    # gradient_accumulation_steps=16,
+    gradient_accumulation_steps=16,
+    gradient_checkpointing=True,
     output_dir="./checkpoint/bert_pre",
     # logging_steps=2,
     # logging_steps=10,
@@ -129,4 +183,4 @@ trainer = Seq2SeqTrainer(
     train_dataset=train_data,
     eval_dataset=val_data,
 )
-# trainer.train()
+trainer.train()
