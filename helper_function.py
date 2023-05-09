3d8dfbb1fe5009459188f790b7e392bdf97bff7c
YuanDang <viviandang121@gmail.com>
2023-05-09 14:57:51 -0400

    model output

---
+++ helper_function.py
@@ -0,0 +1,23 @@
+import numpy as np
+import random
+import torch
+from pynvml import *
+
+def set_seeds(seed):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+
+
+def print_gpu_utilization():
+    nvmlInit()
+    handle = nvmlDeviceGetHandleByIndex(0)
+    info = nvmlDeviceGetMemoryInfo(handle)
+    print(f"GPU memory occupied: {info.used // 1024 ** 2} MB.")
+
