5346d606ab51e64db6b036926b5e2f4eebdb89fc
YuanDang <viviandang121@gmail.com>
2023-05-09 15:00:37 -0400

    update dataset

--- clean_data.py
+++ clean_data.py
@@ -17,11 +17,12 @@ PATH = 'data-csv/'
 
 # ----------read data
 df_raw = pd.read_csv(PATH+'randomdata.csv', header=0, index_col=[0])
-key_col = ['overall', 'reviewText', 'summary', 'type']
+key_col = ['overall', 'reviewText', 'summary', 'type', 'asin']
 df = df_raw[key_col]
 
 # ----------check null value
-print('missing values exists:\n', df.isnull().sum())
+print('missing values exists:\n')
+print(df.isnull().sum())
 # remove null
 df = df.dropna()
 
@@ -68,16 +69,18 @@ raw_txt_len = df.text.apply(lambda x: len(x.split()))
 sum_len = df.summary.apply(lambda x: len(x.split()))
 
 plt.figure()
-plt.hist(raw_txt_len, bins=100)
+plt.hist(raw_txt_len, bins=40, color='skyblue')
 plt.title('Review Token Length')
 plt.xticks(np.arange(0, 900, 100))
+# plt.xlabel(np.arange(0, 900, 100))
+plt.xlim((0, 900))
 plt.xlabel('length')
 plt.ylabel('count')
 plt.grid(axis='x')
 plt.show()
 
 plt.figure()
-plt.hist(sum_len, bins=20)
+plt.hist(sum_len, bins=20, color='skyblue')
 plt.title('Summary Token Length')
 plt.xticks(np.arange(0, 100, 10))
 plt.xlabel('length')
@@ -95,11 +98,12 @@ def rm_long(x, max_len=1000):
     """
     words = x.split()
     if len(words) > max_len:
-        return words[:max_len]
+        truncate = words[:max_len]
+        return " ".join(truncate)
     else:
         return x
 
-df['text'] = df['text'].astype(str).apply(rm_long, max_len=300)
+df['text'] = df['text'].astype(str).apply(rm_long, max_len=1000)
 # df['words'] = df['words'].astype(str).apply(rm_long)
 # ----------remove non-sense text
 print('Dataset has text with no sense:')
@@ -126,13 +130,15 @@ print(f'shape of cleaned data:{df.shape}')
 # ----------train test split
 SEED=666
 TEST_RATIO = 0.2
-N = 40000
+# N = 50000
+N = len(df)
 OUTPUT_PATH = 'clean_data/'
 df_sub = df.sample(N, random_state=SEED)
+df_asin = df[['text','summary','asin']].groupby('asin').agg({'text': lambda x: " ".join(x), 'summary': lambda x: ". ".join(x)})
 train_set = df_sub.iloc[:int(len(df_sub) * (1 - TEST_RATIO))][['text', 'summary']]
-test_set = df_sub.iloc[-int(len(df_sub) * TEST_RATIO):][['text', 'summary']]
+test_set = df_sub.iloc[-int(len(df_sub) * TEST_RATIO):][['text', 'summary', 'asin']]
 print(f'shape of train data:{train_set.shape}')
 print(f'shape of test data:{test_set.shape}')
 train_set.to_csv(OUTPUT_PATH+'train.csv')
 test_set.to_csv(OUTPUT_PATH+'test.csv')
-
+df_asin.to_csv(OUTPUT_PATH+'item_reviews.csv')
